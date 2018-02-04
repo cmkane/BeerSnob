cee33cfc62d16d0a2d9afabdc661c1460de2285d
Carissa Kane <cmkane@uci.edu>
2018-02-04 22:34:06 +0000

    Add S3 Upload to takePic

--- takePic.sh
+++ takePic.sh
@@ -1,2 +1,19 @@
+# Set up PATH for AWS CLI
+source ~/.profile
+
+# Take pic for both cameras after 5 second sleep
 sleep 5
-fswebcam image.jpg
+while [ ! -f "image.jpg" ]
+      do
+	  fswebcam -d /dev/video0 --no-banner image.jpg
+done
+
+fswebcam -d /dev/video1 --no-banner -r 1920x1080 image2.jpg
+
+# Rename files for check
+mv image.jpg SmallCamDoor.jpg
+mv image2.jpg BigCamTop.jpg
+
+# Upload image files to S3
+aws s3 cp "/home/pi/CamImages/SmallCamDoor.jpg" s3://iotminifridge/
+aws s3 cp "/home/pi/CamImages/BigCamTop.jpg" s3://iotminifridge/
